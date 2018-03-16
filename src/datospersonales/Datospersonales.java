f2d5a7e048be47f014a5c96730eec433c3c4353c
Monica <>
2018-03-16 16:23:49 -0600

    Completo

---
+++ src/datospersonales/Datospersonales.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datospersonales;
+
+/**
+ *
+ * @author ufg
+ */
+public class Datospersonales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("nombre: Monica Guadalupe Gomez Vilanova\n"
+                + "Edad: 20 anios\n"
+                + "Nacimiento: 01 de febrero de 1998\n"
+                + "Carnet: 0008616\n"
+                + "Tipo de sangre: ORH+\n"
+                + "Sexo: Femenino\n"
+                + "Estado civil: Soltera\n");
+        // TODO code application logic here
+    }
+    
+}
